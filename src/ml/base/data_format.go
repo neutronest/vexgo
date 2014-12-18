1f0a646c67eb2724cd5027059164c8011d5da801
dongchao <dongchao@xiaomi.com>
2014-12-18 17:55:53 +0900

    layer dev

--- src/ml/base/data_format.go
+++ src/ml/base/data_format.go
@@ -1,5 +1,10 @@
 package base
 
+import (
+	"Math"
+	"base/"
+)
+
 type DataType float64
 
 //- ----------------------- Data Frame
@@ -21,13 +26,41 @@ type DataVol struct {
 
 	depth int
 
-	w float64
+	w []float64
 
-	dw float64
+	dw []float64
 }
 
-func (dataVol *DataVol) init(sx int, sy int, depth int, weight float64) {
+func (dataVol *DataVol) init(sx int, sy int, depth int) {
 
-	dataVol.sx = 1
-	return nil
+	if sx == 1 {
+
+		dataVol.sx = 1
+		dataVol.sy = 1
+		dataVol.depth = depth
+
+		var wArray, dwArray [depth]float64
+		dataVol.w = wArray
+		dataVol.dw = dwArray
+		for i := 0; i < dataVol.depth; i++ {
+
+			dataVol.w[i] = sx[i]
+		}
+	} else {
+
+		dataVol.sx = sx
+		dataVol.sy = sy
+		dataVol.depth = depth
+		var n = sx * sy * depth
+		var wArray, dwArray [n]float64 // TODO Optimization
+		dataVol.w = wArray
+		dataVol.dw = dwArray
+
+		var scale = Math.Sart(1.0 / n)
+		for i := 0; i < n; i++ {
+
+			dataVol.w[i] = GetRandn(0.0, scale)
+		}
+	}
+
 }
